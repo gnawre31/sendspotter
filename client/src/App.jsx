116d064d62d255cd55dd2d345042f45713cf2d1d
Erwang Li <erwangli2@gmail.com>
2023-11-07 08:30:24 -0500

    added google tag

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import { useEffect } from "react"
 import { useShoeStore } from "./store"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import Home from "./pages/Home"
 import PDP from "./pages/PDP"
-import Nav from "./components/Nav"
+// import Nav from "./components/Nav"
+import ReactGA from 'react-ga';
 
 const App = () => {
   const getAllShoes = useShoeStore(state => state.getAllShoes)
+
+  ReactGA.initialize('G-99PB07TDMW');
+
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
 
 
   useEffect(() => {
@@ -16,14 +24,13 @@ const App = () => {
 
 
   return (
-    <BrowserRouter>
-      {/* <Nav /> */}
+    <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shoe/:id" element={<PDP />} />
 
       </Routes>
-    </BrowserRouter>
+    </div>
   )
 }
 
