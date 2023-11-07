ca72d8ebbbbffc6ecf07c0904523683f43af69a4
Erwang Li <erwangli2@gmail.com>
2023-11-07 08:38:34 -0500

    shortened search bar

--- client/src/components/catalogue/Filter.jsx
+++ client/src/components/catalogue/Filter.jsx
@@ -8,16 +8,19 @@ const Filter = () => {
 
 
     return (
-        <div className='w-[800px] bg-green-500 h-12 rounded-xl'>
-            <form className='w-full h-full'>
-                <input type="text"
-                    value={searchValue}
-                    onChange={e => setSearch(e.target.value)}
-                    className='w-full h-full p-4 border rounded-xl'
-                    placeholder='Search...'
-                />
-            </form>
+        <div className='w-[800px]'>
+            <div className='w-3/5 bg-green-500 h-12 rounded-xl'>
+                <form className='w-full h-full'>
+                    <input type="text"
+                        value={searchValue}
+                        onChange={e => setSearch(e.target.value)}
+                        className='w-full h-full p-4 border rounded-xl'
+                        placeholder='Search...'
+                    />
+                </form>
+            </div>
         </div>
+
     )
 }
 
